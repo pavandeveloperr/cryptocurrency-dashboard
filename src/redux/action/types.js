8a921f76fbab3f24007c5fe326f1dd27c15fe0b9
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2022-12-03 20:24:08 +0530

    setup redux

---
+++ src/redux/action/types.js
@@ -0,0 +1,8 @@
+const actionTypes = {
+
+    INITIALIZE: 'INITIALIZE',
+    COIN_API_SUCCESS: 'COIN_API_SUCCESS',
+    COIN_API_ERROR: 'COIN_API_ERROR'
+}
+
+export default actionTypes
---
+++ src/redux/reducer/defaultReducer.js
@@ -0,0 +1,28 @@
+import actionTypes from "../action/types";
+
+const initialState ={
+    projectName:"Crypto dashboard",
+    coinList: []
+}
+
+const defaultReducer = (state=initialState, action) =>{
+    
+    switch(action.type){
+
+
+        case actionTypes.COIN_API_SUCCESS:
+            return {
+                ...state,
+                coinList : action.payload
+
+            }
+
+        case actionTypes.COIN_API_ERROR:
+            alert(action.payload)
+            return state
+
+        default: return state
+    }
+}
+
+export default defaultReducer;
