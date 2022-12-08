9507e80971358e9f588ab5a3db9d5ddcc3a85ee5
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2022-12-08 17:58:20 +0530

    feat : add exchangeCoins feature using redux

--- src/redux/action/action.js
+++ src/redux/action/action.js
@@ -20,3 +20,23 @@ export const fetchCoins = () => {
           })
       }
   }
+
+export const fetchCoinList = () => {
+  return (dispatch) => {
+    axios.get('https://api.coingecko.com/api/v3/exchange_rates')
+    .then(response =>{
+      const data = response.data
+      dispatch({
+        type: actionTypes.EXCHANGE_SUCCESS,
+        payload: data
+      })
+    })
+      .catch(error => {
+        const errorMsg = error.message 
+        dispatch({
+          type: actionTypes.EXCHANGE_ERROR,
+          payload: errorMsg  
+      })
+    })
+  }
+}
--- src/redux/reducer/rootReducer.js
+++ src/redux/reducer/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import defaultReducer from "./defaultReducer";
+import exchangeReducer from "./exchangeReducer";
 
 const rootReducer = combineReducers ({
-    default:defaultReducer,
+    default : defaultReducer,
+    exchange : exchangeReducer,
 })
 
 
