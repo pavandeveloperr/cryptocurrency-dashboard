020e5ef9ba041ce3f0e4e564331f95188e778b5e
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2022-12-20 22:29:38 +0530

    feat: add tailwind scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,7 +30,7 @@ module.exports = {
     //   xl: "1280px",
     //   // => @media (min-width: 1280px) { ... }
     // },
-    plugins: [],
+    plugins: [require("tailwind-scrollbar")],
   },
 };
  
