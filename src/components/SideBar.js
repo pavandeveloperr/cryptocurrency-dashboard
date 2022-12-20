232dee9bf3d30ce03e107d0e2bdf03b3b68ee189
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2022-12-20 22:32:40 +0530

    feat : add reset button

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -4,8 +4,7 @@ import selectIcon from "../assets/select-icon.svg";
 import Pagination from "./Pagination";
 
 export const SideBar = () => {
-  const { cryptoData } = useContext(CryptoContext);
-  const { setSortBy } = useContext(CryptoContext);
+  const { cryptoData, setSortBy, resetFunction } = useContext(CryptoContext);
 
   return (
     <div className="bg-white rounded-lg shadow-lg">
@@ -19,7 +18,7 @@ export const SideBar = () => {
         <select
           name="sortby"
           onChange={(e) => setSortBy(e.target.value)}
-          className="rounded bg-gray-100 text-[14px] mr-2 pr-4 pl-2 py-0.5 capitalize focus:outline-0 cursor-pointer"
+          className="rounded bg-gray-100 text-[14px] mr-24 pr-4 pl-2 py-0.5 capitalize focus:outline-0 cursor-pointer"
         >
           <option value="market_cap_desc">market cap desc</option>
           <option value="market_cap_asc">market cap asc</option>
@@ -34,28 +33,53 @@ export const SideBar = () => {
           src={selectIcon}
           alt="submit"
           className="w-[0.7rem] h-auto absolute 
-          right-7 pointer-events-none"
+          right-[10.8rem] pointer-events-none"
         />
+
+      <button
+          className="w-[1.6rem] flex ml-8 hover:scale-110 transition-all transition-ease"
+          onClick={resetFunction}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+            className="w-full h-full fill-gray-600"
+            style={{
+              msTransform: "rotate(360deg)",
+              WebkitTransform: "rotate(360deg)",
+              transform: "rotate(360deg)",
+            }}
+          >
+            <path d="M12 16c1.671 0 3-1.331 3-3s-1.329-3-3-3-3 1.331-3 3 1.329 3 3 3z" />
+            <path d="M20.817 11.186a8.94 8.94 0 0 0-1.355-3.219 9.053 9.053 0 0 0-2.43-2.43 8.95 8.95 0 0 0-3.219-1.355 9.028 9.028 0 0 0-1.838-.18V2L8 5l3.975 3V6.002c.484-.002.968.044 1.435.14a6.961 6.961 0 0 1 2.502 1.053 7.005 7.005 0 0 1 1.892 1.892A6.967 6.967 0 0 1 19 13a7.032 7.032 0 0 1-.55 2.725 7.11 7.11 0 0 1-.644 1.188 7.2 7.2 0 0 1-.858 1.039 7.028 7.028 0 0 1-3.536 1.907 7.13 7.13 0 0 1-2.822 0 6.961 6.961 0 0 1-2.503-1.054 7.002 7.002 0 0 1-1.89-1.89A6.996 6.996 0 0 1 5 13H3a9.02 9.02 0 0 0 1.539 5.034 9.096 9.096 0 0 0 2.428 2.428A8.95 8.95 0 0 0 12 22a9.09 9.09 0 0 0 1.814-.183 9.014 9.014 0 0 0 3.218-1.355 8.886 8.886 0 0 0 1.331-1.099 9.228 9.228 0 0 0 1.1-1.332A8.952 8.952 0 0 0 21 13a9.09 9.09 0 0 0-.183-1.814z" />
+            <path fill="rgba(0, 0, 0, 0)" d="M0 0h24v24H0z" />
+          </svg>
+        </button>
+
+        
       </label>
+      
       <div className="mt-4">
         {cryptoData ? (
           <table className="w-full table-auto">
             <tbody>
               {cryptoData.map((cryptoData) => {
                 return (
-                  <tr
+                  <div
                     key={cryptoData.id}
                     className="text-center text-lg border-b border-gray-100 hover:bg-gray-200 last:border-b-0"
                   >
                     <img
                       src={cryptoData.image}
                       alt={cryptoData.name}
-                      className="flex absolute w-[1.1rem] h-[1.1rem] ml-3 mt-4"
+                      className="flex absolute w-[1.1rem] h-[1.1rem] ml-3 mt-2"
                     />
 
-                    <td className="flex flex-row pl-9 mt-2 text-[13px] font-semibold">
+                    <span className="flex flex-row pl-9 mt-2 text-[13px] font-semibold">
                       {cryptoData.name}
-                    </td>
+                    </span>
 
                     <div className="flex flex-row justify-end mr-5">
                       <div
@@ -89,7 +113,7 @@ export const SideBar = () => {
                         }).format(cryptoData.market_cap)}
                       </span>
                     </div>
-                  </tr>
+                  </div>
                 );
               })}
             </tbody>
