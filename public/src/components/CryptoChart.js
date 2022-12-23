750cd2c4098cfe13c1e2c064ab5cda54ca923268
Pavan Kulkarni <pavank.bit@gmail.com>
2022-12-24 01:41:48 +0530

    Delete public/src directory

--- public/src/components/CryptoChart.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const CryptoChart = () => {
-  return <div className="h-96 border my-2">CryptoChart2</div>;
-};
--- public/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from 'redux-devtools-extension';
-import logger from 'redux-logger'
-import rootReducer from "./reducer/rootReducer";
-
-const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(logger,thunk)))
-
-export default store;
