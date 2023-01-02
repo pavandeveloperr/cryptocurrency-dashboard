791918ceebbd422c210b7c3115489684602af4a8
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-01-02 17:11:24 +0530

    refactor : add new heart emoji

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,8 +5,7 @@ export const Footer = () => {
     <footer class="relative text-center text-black-400 lg:text-center md:text-center sm:text-center">
         <div className="my-4">
           <h3 className="footer-content font-semibold text-gray-300">
-            Made with{" "}
-            <i className="fas fa-heart text-red-600" aria-hidden="true"></i>{" "}
+            Made with{" "}❤️{" "}
             <a
               class="link"
               href="https://github.com/pavandeveloperr"
